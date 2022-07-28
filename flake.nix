c662dac11934157c243f8b8864904edd562d39e4
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-07-28 20:20:04 +0300

    effects: add an effect for deploying ardana tenant

--- flake.nix
+++ flake.nix
@@ -4,8 +4,17 @@
     nixinate.url = "github:matthewcroughan/nixinate";
     danaswapstats.url = "git+ssh://git@github.com/ArdanaLabs/danaswapstats";
     dana-circulating-supply.url = "git+ssh://git@github.com/ArdanaLabs/dana-circulating-supply?ref=main";
+    hci-effects.url = "github:hercules-ci/hercules-ci-effects";
   };
-  outputs = { self, nixpkgs, danaswapstats, dana-circulating-supply, nixinate, ... }@inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    danaswapstats,
+    dana-circulating-supply,
+    nixinate,
+    hci-effects,
+    ...
+  }@inputs: {
     apps = nixinate.nixinate.x86_64-linux self;
     nixosConfigurations = {
       # tenant is the co-located machine that Isaac provisioned. It is running
@@ -28,6 +37,25 @@
           }
         ];
         specialArgs = { inherit inputs; };
+      };
+    };
+    herculesCI = { branch, ... }: {
+      onPush.default = {
+        outputs.effects = {
+          ardanaTenantDeploy =
+            let
+              pkgs = nixpkgs.legacyPackages."x86_64-linux";
+              effects = hci-effects.lib.withPkgs pkgs;
+            in
+              effects.mkEffect {
+                effectScript = ''
+                  # deploy tenant (dry run)
+                  ${self.apps.nixinate.tenant-dry-run}
+                  # deploy tenant
+                  ${self.apps.nixinate.tenant}
+                '';
+              };
+        };
       };
     };
   };
