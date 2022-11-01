055547e2035563cda20fa06f6559e3886c55de0e
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-11-01 13:22:15 -0500

    add checks to hci-effects

--- flake.nix
+++ flake.nix
@@ -51,8 +51,9 @@
     in
     {
       checks.x86_64-linux.format =
-        let pkgs =
-          nixpkgs.legacyPackages.x86_64-linux;
+        let
+          pkgs =
+            nixpkgs.legacyPackages.x86_64-linux;
         in
         pkgs.runCommand "check-fmt" { } ''
           set -euo pipefail
@@ -64,64 +65,61 @@
       nixosConfigurations = {
         tenant = mkTenantSystem inputs;
       };
-      herculesCI = { branch, ... }: {
-        onPush.default = {
-          outputs.effects = {
-            ardanaTenantDeploy =
-              let
-                system = "x86_64-linux";
-                pkgs = nixpkgs.legacyPackages.${system};
-                effects = hci-effects.lib.withPkgs pkgs;
-                inherit (effects) runIf mkEffect;
+      effects = { branch, ... }: {
+        ardanaTenantDeploy =
+          let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+            effects = hci-effects.lib.withPkgs pkgs;
+            inherit (effects) runIf mkEffect;
 
-                # We have to do this so that we avoid using the SSH inputs.
-                # These require a SSH key. Instead we can just make use of the netrc
-                # file with HTTPS which is also recommended by Hercules.
-                # (https://docs.hercules-ci.com/hercules-ci-agent/netrc/#git-ssh)
-                tenant =
-                  let
-                    # Read the flake.lock so we can get the information we need
-                    # Eg. Git revisions and NAR hashes.
-                    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes;
-                    # Define some helper functions to create a flake input.
-                    # The main thing here is `builtins.getFlake` which will fetch
-                    # the flake, and the result will be usable as a flake input.
-                    mkInput = url: node:
-                      builtins.getFlake "${url}/${node.rev}?narHash=${node.narHash}";
-                    mkArdanaInput = name:
-                      mkInput "github:ArdanaLabs/${name}" lock.${name}.locked;
-                  in
-                  mkTenantSystem (
-                    inputs // {
-                      danaswapstats = mkArdanaInput "danaswapstats";
-                      dana-circulating-supply = mkArdanaInput "dana-circulating-supply";
-                    }
-                  );
-                # Create the deploy apps using nixinate.
-                deployApps =
-                  nixinate.nixinate.${system}
-                    (self // { nixosConfigurations.tenant = tenant; });
+            # We have to do this so that we avoid using the SSH inputs.
+            # These require a SSH key. Instead we can just make use of the netrc
+            # file with HTTPS which is also recommended by Hercules.
+            # (https://docs.hercules-ci.com/hercules-ci-agent/netrc/#git-ssh)
+            tenant =
+              let
+                # Read the flake.lock so we can get the information we need
+                # Eg. Git revisions and NAR hashes.
+                lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes;
+                # Define some helper functions to create a flake input.
+                # The main thing here is `builtins.getFlake` which will fetch
+                # the flake, and the result will be usable as a flake input.
+                mkInput = url: node:
+                  builtins.getFlake "${url}/${node.rev}?narHash=${node.narHash}";
+                mkArdanaInput = name:
+                  mkInput "github:ArdanaLabs/${name}" lock.${name}.locked;
               in
-              # Only run this effect on master branch.
-                # The CI will build the dependencies of this effect even if it won't run.
-              runIf (branch == "master") (mkEffect {
-                inputs = [ pkgs.openssh pkgs.git ];
-                secretsMap.ssh = "tenantDeployKeys";
-                userSetupScript = ''
-                  writeSSHKey ssh
-                  cat >>~/.ssh/known_hosts <<EOF
-                    216.24.131.4 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkK+eMwud2J9oYUhIWfmDRsfn1DfaGxtt3EM/BUPmcp
-                  EOF
-                '';
-                effectScript = ''
-                  # deploy tenant (dry run)
-                  ${deployApps.nixinate.tenant-dry-run.program}
-                  # deploy tenant
-                  ${deployApps.nixinate.tenant.program}
-                '';
-              });
-          };
-        };
+              mkTenantSystem (
+                inputs // {
+                  danaswapstats = mkArdanaInput "danaswapstats";
+                  dana-circulating-supply = mkArdanaInput "dana-circulating-supply";
+                }
+              );
+            # Create the deploy apps using nixinate.
+            deployApps =
+              nixinate.nixinate.${system}
+                (self // { nixosConfigurations.tenant = tenant; });
+          in
+          # Only run this effect on master branch.
+            # The CI will build the dependencies of this effect even if it won't run.
+          runIf (branch == "master") (mkEffect {
+            inputs = [ pkgs.openssh pkgs.git ];
+            secretsMap.ssh = "tenantDeployKeys";
+            userSetupScript = ''
+              writeSSHKey ssh
+              cat >>~/.ssh/known_hosts <<EOF
+                216.24.131.4 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkK+eMwud2J9oYUhIWfmDRsfn1DfaGxtt3EM/BUPmcp
+              EOF
+            '';
+            effectScript = ''
+              # deploy tenant (dry run)
+              ${deployApps.nixinate.tenant-dry-run.program}
+              # deploy tenant
+              ${deployApps.nixinate.tenant.program}
+            '';
+          });
       };
+      herculesCI.ciSystems = [ "x86_64-linux" ];
     };
 }
